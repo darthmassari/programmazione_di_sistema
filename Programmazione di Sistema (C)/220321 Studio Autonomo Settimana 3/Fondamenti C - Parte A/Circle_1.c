d67f09cc4d85ac6cd625388118b1f3164aba41d0
marco-ciucci <94639044+marco-ciucci@users.noreply.github.com>
2022-06-26 18:30:21 +0200

    Aggiunta dei codici di LPS

---
+++ Programmazione di Sistema (C)/220321 Studio Autonomo Settimana 3/Fondamenti C - Parte A/Circle_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define PI 3.14159f
+
+int main (void) {
+	float raggio;
+	printf("Inserisci raggio: ")
+	scanf("%f", &raggio);
+
+	printf("La circonferenza è: %2f", 2 *PI * raggio);
+	printf("L'area è: %2f", PI * raggio * raggio);
+
+	return 0;
+}
---
+++ Programmazione di Sistema (C)/220404 Esercitazione 4/Traduzione da C ad Assembly/code_c.c
@@ -0,0 +1,9 @@
+int s1 = 23, s2 = 9, s3 = -11;
+
+int main(void) {
+	s2 = s1 + s2 +4;
+	s3 = s2 - (14 - s1);
+	s1 = s2 - s3 - 15;
+
+	return 0;
+}
