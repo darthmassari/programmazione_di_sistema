1d4a229c572fb83ea981ed2f211384e125b4fb66
marco-ciucci <94639044+markooo384@users.noreply.github.com>
2022-06-27 20:26:51 +0200

    nuovi esercizi

--- Programmazione di Sistema (C)/220411 Esercizi Fondamenti ASM/4_descrizione_comandi_mips.asm
+++ Programmazione di Sistema (C)/220411 Esercizi Fondamenti ASM/4_descrizione_comandi_mips.asm
@@ -8,12 +8,16 @@
 
 5. add $t6,$t1	#non valida, add vuole 3 operandi
 
-6. add $t7,100,$t2	#valida, somma 100 e il contenuto di t2 e 						#lo memorizza in t7
+6. add $t7,100,$t2	#valida, somma 100 e il contenuto di t2 e 						
+					#lo memorizza in t7
 
 7. sub $t6,$t1,$t2	#valida, guarda su
 
-8. mult $t5,$t2,$a1	#non valida, mult vuole 2 operandi, altrimenti					  #mul vuole 3 operandi
+8. mult $t5,$t2,$a1	#non valida, mult vuole 2 operandi, altrimenti
+					#mul vuole 3 operandi
 
-9. div $t3,$t2,4 	#valida, il quoziente va nel primo operand, il					  #resto viene perduto
+9. div $t3,$t2,4 	#valida, il quoziente va nel primo operand, il
+					#resto viene perduto
 
-10. div $t6,$s2	#valida, quoziente e resto vengono memorizzati in				 #LO e in HI rispettivamente
+10. div $t6,$s2	#valida, quoziente e resto vengono memorizzati in
+				#LO e in HI rispettivamente
